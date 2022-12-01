055bca8f3a2ed8dc5c5ad8731d614a4287dba7a2
s-1117 <119517083+s-1117@users.noreply.github.com>
2022-12-02 01:15:31 +0900

    Add files via upload

---
+++ 1-1/Task1_1.java
@@ -0,0 +1,24 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        int hogeInt = 50;
+
+        String hogeString = "テスト";
+
+        //double型のcceptanceRateと言う変数に対して初期値を決める処理
+        double hogeDouble = 0.2;
+
+        hogeInt =100;
+
+        hogeString = "合格";
+
+        System.out.println(hogeInt);
+
+        // hogeStringと表示させる指示を出す処理
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+
+   }
+}
